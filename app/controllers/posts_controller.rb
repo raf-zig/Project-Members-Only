4c4875cf02667757cebcfcf57dfb54ecad886e5a
raf-zig <ziravin@yandex.ru>
2023-07-29 23:11:22 +0500

    Add methods to  post_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,27 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create 
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else  
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :posts, only: [:index, :new, :create]
   # Defines the root path route ("/")
-  #root "posts#index"
+  root "posts#index"
   
 end
